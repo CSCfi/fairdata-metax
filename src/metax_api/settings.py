33805e11578916993dd0844d11f1faa6d29d8dd0
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-05-11 16:45:04 +0300

    Change location of static files to same dir as manage.py

--- src/metax_api/settings.py
+++ src/metax_api/settings.py
@@ -147,5 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# same dir as manage.py
+STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_DIR), 'static')
 STATIC_URL = '/static/'
