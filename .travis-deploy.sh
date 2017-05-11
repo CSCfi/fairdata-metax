738d263bd7f9cd4f27ad6764a21d87bc60b4f0bf
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-05-11 16:59:01 +0300

    debugging

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -6,5 +6,7 @@ if [[ "$TRAVIS_BRANCH" == "dev" ]]; then
     git clone https://github.com/CSCfi/metax-ops
     cd metax-ops/ansible/ && ansible-playbook -vv -i environments/development deploy.yml
 elif [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "Staging"
 elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "Master"
 fi
