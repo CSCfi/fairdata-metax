120ab7b30fa66cb28f651c076bc92fbc5c81b577
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-05-15 16:01:04 +0300

    Change playbook to run on deploy.

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -4,7 +4,7 @@
 echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then
     git clone https://github.com/CSCfi/metax-ops
-    cd metax-ops/ansible/ && ansible-playbook -vv -i environments/development deploy.yml
+    cd metax-ops/ansible/ && ansible-playbook -vv -i environments/development site_deploy.yml
 elif [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Staging"
 elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
