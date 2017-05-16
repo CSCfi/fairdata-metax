2218a609c3a6e6036e9a17de0e2785d7c0e2521a
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-05-16 14:32:22 +0300

    Change travis-deploy script to reflect new github branches

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -xeu
 
 # Run as the after_success step of a travis build.
-echo $TRAVIS_BRANCH
-if [[ "$TRAVIS_BRANCH" == "dev" ]]; then
-    git clone https://github.com/CSCfi/metax-ops
-    cd metax-ops/ansible/ && ansible-playbook -vv -i environments/development site_deploy.yml
-elif [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+git clone https://github.com/CSCfi/metax-ops
+cd metax-ops/ansible/
+
+if [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Staging"
+    ansible-playbook -vv -i environments/staging site_deploy.yml
+elif [[ "$TRAVIS_BRANCH" == "stable" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "Stable"
 elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Master"
 fi
