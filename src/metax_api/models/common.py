98c71340b5243449c60815c92ba6316f32de5975
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-05-29 13:01:29 +0300

    [ADD] CSCMETAX-35: Update file json schema and django models

--- src/metax_api/models/common.py
+++ src/metax_api/models/common.py
@@ -2,17 +2,36 @@ from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import User
 
+
+import logging
+_logger = logging.getLogger(__name__)
+d = logging.getLogger(__name__).debug
+
 class Common(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    identifier_sha256 = models.DecimalField(max_digits=79, decimal_places=0, blank=False, unique=True)
     active = models.BooleanField(default=True)
-    modified_by_api = models.DateTimeField(auto_now=True)
-    modified_by_user_id = models.ForeignKey(User, related_name='modified_by_user', null=True)
-    created_by_api = models.DateTimeField(auto_now_add=True)
-    created_by_user_id = models.ForeignKey(User, related_name='creataed_by_user', null=True)
+    removed = models.BooleanField(default=False)
+    modified_by_api = models.DateTimeField(null=True)
+    modified_by_user_id = models.ForeignKey(User, related_name='%(class)s_modified_by_user',
+                                            null=True, db_column='modified_by_user_id')
+    created_by_api = models.DateTimeField()
+    created_by_user_id = models.ForeignKey(User, related_name='%(class)s_created_by_user',
+                                           null=True, db_column='created_by_user_id')
 
     class Meta:
         abstract = True
+        indexes = [
+            models.Index(fields=['identifier_sha256']),
+        ]
+
+    def delete(self):
+        """
+        Mark record as removed, never delete from db.
+        """
+        self.removed = True
+        self.save()
 
     def __str__(self):
         return str(self.id)
--- src/metax_api/models/file.py
+++ src/metax_api/models/file.py
@@ -3,22 +3,25 @@ from django.db import models
 
 from .common import Common
 
+class FileStorage(Common):
+
+    title = models.CharField(max_length=200, blank=True)
+    access_url = models.CharField(max_length=200, blank=True)
+
 class File(Common):
 
-    # todo add to serializer
-
-    access_group = models.CharField(max_length=200, blank=True)
+    access_group = models.CharField(max_length=200)
     byte_size = models.PositiveIntegerField(default=0)
-    checksum_algorithm = models.CharField(max_length=200, blank=True)
+    checksum_algorithm = models.CharField(max_length=200)
     checksum_checked = models.DateTimeField(null=True)
-    checksum_value = models.CharField(max_length=200, blank=True)
-    download_url = models.URLField(blank=True)
-    file_format = models.CharField(max_length=200, blank=True)
+    checksum_value = models.CharField(max_length=200)
+    download_url = models.URLField()
+    file_format = models.CharField(max_length=200)
     file_modified = models.DateTimeField(auto_now=True)
-    file_name = models.CharField(max_length=64, blank=True, null=True)
-    # file_storage_id = models.ForeignKey('FileStorage')
-    file_path = models.CharField(max_length=200, blank=True)
-    json = JSONField(blank=True, null=True)
+    file_name = models.CharField(max_length=64)
+    file_storage_id = models.ForeignKey('FileStorage', null=True, db_column='file_storage_id')
+    file_path = models.CharField(max_length=200)
+    identifier = models.CharField(max_length=200, unique=True)
+    file_characteristics = JSONField(blank=True, null=True)
     open_access = models.BooleanField(default=False)
-    removed = models.BooleanField(default=False)
-    replication_path = models.CharField(max_length=200, blank=True)
+    replication_path = models.CharField(max_length=200, blank=True, null=True)
