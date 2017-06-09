9d6f313c81b468008c38c4f623a9ffb8987498d4
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-06-09 16:29:43 +0300

    CSCMETAX-45: [ADD] Update dataset model fields

--- src/metax_api/models/dataset.py
+++ src/metax_api/models/dataset.py
@@ -7,8 +7,8 @@ from .dataset_catalog import DatasetCatalog
 class Dataset(Common):
 
     identifier = models.CharField(max_length=200, unique=True)
-    dataset_json = JSONField(blank=True, null=True)
-    dataset_catalog_id = models.ForeignKey(DatasetCatalog, db_column='dataset_catalog_id', null=True, blank=True)
+    dataset_json = JSONField()
+    dataset_catalog_id = models.ForeignKey(DatasetCatalog, db_column='dataset_catalog_id')
 
     indexes = [
         models.Index(fields=['identifier']),
