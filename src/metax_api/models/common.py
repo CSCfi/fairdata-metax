b4a5fcc557180018daf119143b7ef53db5f54069
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-06-27 13:06:39 +0300

    CSCMETAX-61: [ADD] Add field tracking option to Common model to detect changes in fields when saving objects

--- src/metax_api/models/common.py
+++ src/metax_api/models/common.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import FieldError
 
 class CommonManager(models.Manager):
 
@@ -28,12 +29,46 @@ class Common(models.Model):
     class Meta:
         abstract = True
 
+    def __init__(self, *args, **kwargs):
+        super(Common, self).__init__(*args, **kwargs)
+        self._initial_data = {}
+
+    def save(self, *args, **kwargs):
+        super(Common, self).save(*args, **kwargs)
+        self._update_tracked_field_values()
+
     def delete(self):
         """
         Mark record as removed, never delete from db.
         """
         self.removed = True
         self.save()
+
+    def track_fields(self, *args):
+        """
+        Save initial values from object fields when object is created (= retrieved from db),
+        so that they can be checked at a later time if the value is being changed or not.
+        """
+        for field_name in args:
+            self._initial_data[field_name] = getattr(self, field_name)
+
+    def field_changed(self, field_name):
+        """
+        Check if a tracked field has changed since last saved to db.
+        """
+        try:
+            initial_value = self._initial_data[field_name]
+        except Exception:
+            raise FieldError('Field %s is not being tracked for changes' % field_name)
+        return getattr(self, field_name) != initial_value
+
+    def _update_tracked_field_values(self):
+        """
+        After saving record to db, tracked field values need to be updated so that
+        field_changed() keeps working as expected
+        """
+        for field_name in self._initial_data.keys():
+            self._initial_data[field_name] = getattr(self, field_name)
 
     def __str__(self):
         return str(self.id)
