272dea996c3f83510e6619238d8b7e62e40af230
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-08-02 19:23:21 +0300

    CSCMETAX-109: [ADD] Add option to force use of master redis node for reading. Add random selection of slave and master for reading

--- src/metax_api/utils/redis.py
+++ src/metax_api/utils/redis.py
@@ -1,4 +1,5 @@
 from pickle import dumps as pickle_dumps, loads as pickle_loads
+from random import choice as random_choice
 
 from django.conf import settings
 from redis.sentinel import Sentinel
@@ -11,7 +12,7 @@ d = logging.getLogger(__name__).debug
 
 class RedisSentinelCache():
 
-    def __init__(self):
+    def __init__(self, master_only=False):
         if not hasattr(settings, 'REDIS_SENTINEL'):
             raise Exception('Missing configuration from settings.py: REDIS_SENTINEL')
         if not settings.REDIS_SENTINEL.get('REDIS_SENTINEL_HOSTS', None):
@@ -25,6 +26,11 @@ class RedisSentinelCache():
             socket_timeout=settings.REDIS_SENTINEL.get('SOCKET_TIMEOUT', 0.1))
         self._service_name = settings.REDIS_SENTINEL['REDIS_SENTINEL_SERVICE']
         self._DEBUG = settings.REDIS_SENTINEL.get('REDIS_SENTINEL_DEBUG', False)
+        self._node_count = self._node_count()
+
+        # always use master for read operations, for those times when you know you are going
+        # to read the same key again from cache very soon
+        self._read_from_master_only = master_only
 
     def set(self, key, value, **kwargs):
         if self._DEBUG:
@@ -48,27 +54,43 @@ class RedisSentinelCache():
                 d('cache: set() unsuccessful, could not get saved data?')
 
     def get(self, key, **kwargs):
-        # todo randomly select slave or master for max profit
+        """
+        Randomly select slave or master for reading. Fallback to master anyway in case of errors.
+
+        Use of master can be forced by using the master_only flag in the constructor.
+        """
+
         # todo allow reading from cache when master is down? could possibly serve stale data.
         # see redis.conf setting: slave-serve-stale-data
         if self._DEBUG:
             d('cache: get()...')
 
-        slave = self._get_slave()
+        if self._read_from_master_only:
+            return self._get_from_master(key, **kwargs)
+        else:
+            if self._slave_chosen():
+                try:
+                    return self._get_from_slave(key, **kwargs)
+                except TimeoutError:
+                    pass
+            # lady luck chose master, or read from slave had an error
+            return self._get_from_master(key, **kwargs)
 
+    def _get_from_slave(self, key, **kwargs):
+        node = self._get_slave()
         try:
-            res = slave.get(key, **kwargs)
+            res = node.get(key, **kwargs)
         except TimeoutError:
             if self._DEBUG:
                 d('cache: slave.get() timed out, trying from master instead. fail-over in process?')
             # fail-over propbably happened, and the old slave is now a master
-            # (in case there was only one slave). # try master instead
-            pass
+            # (in case there was only one slave). try master instead
+            raise
         else:
             return pickle_loads(res) if res is not None else None
 
+    def _get_from_master(self, key, **kwargs):
         master = self._get_master()
-
         try:
             res = master.get(key, **kwargs)
         except (TimeoutError, MasterNotFoundError):
@@ -77,8 +99,10 @@ class RedisSentinelCache():
             # uh oh, no master available either. either all redis instances have hit the bucket,
             # or there is a fail-over in process, and a new master will be in line in a moment
             return None
-
         return pickle_loads(res) if res is not None else None
+
+    def _slave_chosen(self):
+        return random_choice(range(0, self._node_count)) != 0
 
     def _get_master(self):
         if self._DEBUG:
@@ -89,3 +113,6 @@ class RedisSentinelCache():
         if self._DEBUG:
             d('cache: getting slave')
         return self._sentinel.slave_for(self._service_name, socket_timeout=0.1)
+
+    def _node_count(self):
+        return len(self._sentinel.discover_slaves(self._service_name)) + 1 # +1 is master
