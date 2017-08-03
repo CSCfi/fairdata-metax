68b413303b953d8ed39f4d93d993e700d50598a4
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-08-03 21:07:22 +0300

    CSCMETAX-110: [REF] Separate ref data update logic away from OnAppStart object so it may be called elsewhere

--- src/metax_api/onappstart.py
+++ src/metax_api/onappstart.py
@@ -1,12 +1,8 @@
 from time import sleep
-
 from django.apps import AppConfig
 from django.conf import settings
-from elasticsearch import Elasticsearch
-from elasticsearch.client import IndicesClient
-from elasticsearch.helpers import scan
 
-from metax_api.utils import RedisSentinelCache
+from metax_api.utils import RedisSentinelCache, executing_test_case, ReferenceDataService
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -19,57 +15,41 @@ class OnAppStart(AppConfig):
 
     def ready(self): # pragma: no cover
         """
-        On application startup, populate cache with reference data from elasticsearch if cache is empty.
+        Execute various tasks during application startup:
+
+        - Populate cache with reference data from elasticsearch if it is missing
+        - If the process is started by a test case, always flush the cache db used by the tests
+
         Usually there are many app processes being ran by gunicorn, but this action should only be executed
-        once. The key 'reference_data_fetch_running' in the cache is set to true by the fastest process,
+        once. The key 'on_app_start_executing' in the cache is set to true by the fastest process,
         informing other processes to not proceed further.
         """
         cache = RedisSentinelCache(master_only=True)
 
-        if settings.ELASTICSEARCH['ALWAYS_RELOAD_CACHE_ON_RESTART']:
-            cache.set('reference_data', None)
-
-        reference_data = cache.get('reference_data')
-
-        if reference_data:
-            # d('cache already populated')
+        if cache.get('on_app_start_executing'):
+            # d('another process is already executing startup tasks, skipping')
             return
 
-        if cache.get('reference_data_fetch_running'):
-            # d('another process is already fetching reference data, skipping')
-            return
+        cache.set('on_app_start_executing', True)
 
-        # no reference data found and no other process yet working on it yet - fetch from elasticsearch
-        cache.set('reference_data_fetch_running', True)
-        _logger.info('Metax API startup - populating cache with reference data...')
-
-        # ensure other processes have stopped at reference_data_fetch_running
+        # ensure other processes have stopped at on_app_start_executing
         # before we reset the flag.
         sleep(2)
 
         # reset the flag right after sleep, to ensure the reset is never prevented by any
-        # unforeseen errors or cosmic rays during fetching, which might block this automatic
-        # fetch on a future restart.
-        cache.set('reference_data_fetch_running', False)
+        # unforeseen errors or cosmic rays during execution, which might block this method on a future restart.
+        cache.set('on_app_start_executing', False)
 
-        esclient = Elasticsearch([{ 'host': settings.ELASTICSEARCH['HOST'], 'port': settings.ELASTICSEARCH['PORT'] }])
-        indicesclient = IndicesClient(esclient)
-        reference_data = {}
+        # actual startup tasks ->
 
-        for index_name, index_info in indicesclient.get_mapping().items():
-            reference_data[index_name] = {}
-            for type_name in index_info['mappings'].keys():
-                all_rows = scan(esclient, query={'query': {'match_all': {}}}, index=index_name, doc_type=type_name)
-                reference_data[index_name][type_name] = [ row['_source']['uri'] for row in all_rows if row['_source']['uri'] ]
+        if executing_test_case():
+            cache.get_master().flushdb()
 
-        cache.set('reference_data', reference_data)
+        if settings.ELASTICSEARCH['ALWAYS_RELOAD_REFERENCE_DATA_ON_RESTART']:
+            cache.set('reference_data', None)
 
-        reference_data_check = cache.get('reference_data')
+        if cache.get('reference_data'):
+            # d('cache already populated')
+            return
 
-        if 'reference_data' not in reference_data_check.keys():
-            _logger.warning('Key reference_data missing from reference data - something went wrong during cache population?')
-
-        if 'organization_data' not in reference_data_check.keys():
-            _logger.warning('Key organization_data missing from reference data - something went wrong during cache population?')
-
-        _logger.info('Metax API startup - cache populated')
+        ReferenceDataService.populate_cache_reference_data(cache)
---
+++ src/metax_api/utils/reference_data_service.py
@@ -0,0 +1,73 @@
+from django.conf import settings as django_settings
+from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
+from elasticsearch.helpers import scan
+
+from .utils import executing_test_case
+
+import logging
+_logger = logging.getLogger(__name__)
+d = logging.getLogger(__name__).debug
+
+class ReferenceDataService():
+
+    """
+    Should optimally be defined in /services/, but services __init__.py cant be loaded during app startup due to having imports from
+    django app models, views etc
+    """
+
+    @classmethod
+    def populate_cache_reference_data(cls, cache, settings=django_settings):
+        """
+        Fetch reference data from elasticsearch server and save to local cache
+
+        cache: cache object to use for saving
+        settings: override elasticsearch settings in settings.py
+        print_stdout: print output also to stdout using print()
+        """
+
+        _logger.info('Metax API startup - populating cache with reference data...')
+
+        if executing_test_case():
+            _logger.info('(Note: populating test suite cache)')
+
+        reference_data = cls._fetch_reference_data(settings)
+
+        cache.set('reference_data', reference_data)
+
+        reference_data_check = cache.get('reference_data')
+
+        if 'reference_data' not in reference_data_check.keys():
+            _logger.warning('Key reference_data missing from reference data - something went wrong during cache population?')
+
+        if 'organization_data' not in reference_data_check.keys():
+            _logger.warning('Key organization_data missing from reference data - something went wrong during cache population?')
+
+        _logger.info('Metax API startup - cache populated')
+
+    @classmethod
+    def _fetch_reference_data(cls, settings):
+        if not isinstance(settings, dict):
+            settings = settings.ELASTICSEARCH
+
+        connection_params = cls._get_connection_parameters(settings)
+        esclient = Elasticsearch(settings['HOSTS'], **connection_params)
+        indicesclient = IndicesClient(esclient)
+        reference_data = {}
+
+        for index_name, index_info in indicesclient.get_mapping().items():
+            reference_data[index_name] = {}
+            for type_name in index_info['mappings'].keys():
+                all_rows = scan(esclient, query={'query': {'match_all': {}}}, index=index_name, doc_type=type_name)
+                reference_data[index_name][type_name] = [ row['_source']['uri'] for row in all_rows if row['_source']['uri'] ]
+
+        return reference_data
+
+    @staticmethod
+    def _get_connection_parameters(settings):
+        """
+        https://docs.objectrocket.com/elastic_python_examples.html
+        """
+        if settings['HOSTS'][0] != 'localhost':
+            return { 'http_auth': (settings['USER'], settings['PASSWORD']), 'port': 443, 'use_ssl': True, 'verify_certs': True }
+        return {}
