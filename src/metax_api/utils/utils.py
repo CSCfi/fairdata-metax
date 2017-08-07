52871a239e77dee1c241b3a40d42f0aa5bc329d1
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-08-07 15:18:38 +0300

    CSCMETAX-61: [ADD] Add utils method to recognize when code is running inside travis

--- src/metax_api/utils/utils.py
+++ src/metax_api/utils/utils.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 def executing_test_case():
@@ -5,3 +6,9 @@ def executing_test_case():
     Returns True whenever code is being executed by automatic test cases
     """
     return 'test' in sys.argv
+
+def executing_travis():
+    """
+    Returns True whenever code is being executed by travis
+    """
+    return True if os.getenv('TRAVIS', False) else False
