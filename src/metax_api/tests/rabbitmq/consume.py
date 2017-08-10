2ec4b70bf63230727a73a725192d6a963095b2ec
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-08-10 12:39:48 +0300

    CSCMETAX-41: [ADD] Add listening for queue 'delete' to rabbitmq test script

--- src/metax_api/tests/rabbitmq/consume.py
+++ src/metax_api/tests/rabbitmq/consume.py
@@ -20,15 +20,18 @@ channel = connection.channel()
 exchange = 'datasets'
 queue_1 = 'testaaja-create'
 queue_2 = 'testaaja-update'
+queue_3 = 'testaaja-delete'
 
 # note: requires write permission to exchanges
 # channel.exchange_declare(exchange=exchange, type='fanout')
 
 channel.queue_declare(queue_1, durable=True)
 channel.queue_declare(queue_2, durable=True)
+channel.queue_declare(queue_3, durable=True)
 
 channel.queue_bind(exchange=exchange, queue=queue_1, routing_key='create')
 channel.queue_bind(exchange=exchange, queue=queue_2, routing_key='update')
+channel.queue_bind(exchange=exchange, queue=queue_3, routing_key='delete')
 
 def callback_1(ch, method, properties, body):
     print(" [ create ] %r" % body)
@@ -36,8 +39,12 @@ def callback_1(ch, method, properties, body):
 def callback_2(ch, method, properties, body):
     print(" [ update ] %r" % body)
 
+def callback_3(ch, method, properties, body):
+    print(" [ delete ] %r" % body)
+
 channel.basic_consume(callback_1, queue=queue_1, no_ack=True)
 channel.basic_consume(callback_2, queue=queue_2, no_ack=True)
+channel.basic_consume(callback_3, queue=queue_3, no_ack=True)
 
 print('[*] Waiting for logs. To exit press CTRL+C')
 channel.start_consuming()
