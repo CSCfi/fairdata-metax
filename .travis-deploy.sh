718152c2a60f5d2de43e9d668f87aa1840cd3c0e
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-08-11 11:54:56 +0300

    CSCMETAX-61: [REF] Set ansible deploy ssh user via command line

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -12,7 +12,7 @@ cd metax-ops/ansible/
 if [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Deploying to staging.."
     ansible-galaxy -r requirements.yml install --roles-path=roles
-    ansible-playbook -vv -i environments/staging site_deploy.yml
+    ansible-playbook -vv -i environments/staging site_deploy.yml --extra-vars "ssh_user=metax-user"
 elif [[ "$TRAVIS_BRANCH" == "stable" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Stable"
 elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
