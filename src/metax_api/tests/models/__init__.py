273f43bedc020a86b115d8f069f3e38473231a09
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-08-31 16:23:15 +0300

    CSCMETAX-158: [ADD] Automatically update research_dataset.total_byte_size when creating new CatalogRecord, or updating research_dataset.files

--- src/metax_api/tests/models/__init__.py
+++ src/metax_api/tests/models/__init__.py
@@ -1,2 +1,2 @@
 from .test_file_model import FileModelBasicTest, FileManagerTests
-from .test_catalog_record_model import CatalogRecordModelBasicTest, CatalogRecordManagerTests
+from .test_catalog_record_model import CatalogRecordModelBasicTest, CatalogRecordModelTests, CatalogRecordManagerTests
--- src/metax_api/tests/models/test_catalog_record_model.py
+++ src/metax_api/tests/models/test_catalog_record_model.py
@@ -28,6 +28,49 @@ class CatalogRecordModelBasicTest(TestCase, TestClassUtils):
         self.assertEqual(catalog_record.urn_identifier, self.urn_identifier)
 
 
+class CatalogRecordModelTests(TestCase, TestClassUtils):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Loaded only once for test cases inside this class.
+        """
+        call_command('loaddata', test_data_file_path, verbosity=0)
+        super(CatalogRecordModelTests, cls).setUpClass()
+
+    def setUp(self):
+        self.cr = CatalogRecord.objects.get(pk=1)
+
+    def test_disallow_total_byte_size_manual_update(self):
+        cr = self.cr
+        old = cr.research_dataset['total_byte_size']
+        cr.research_dataset['total_byte_size'] = 999
+        cr.save()
+        self.assertEqual(old, cr.research_dataset['total_byte_size'])
+
+    def test_disallow_urn_identifier_manual_update(self):
+        cr = self.cr
+        old = cr.research_dataset['urn_identifier']
+        cr.research_dataset['urn_identifier'] = 'changed'
+        cr.save()
+        self.assertEqual(old, cr.research_dataset['urn_identifier'])
+
+    def test_total_byte_size_auto_update(self):
+        file_from_testdata = self._get_object_from_test_data('file', requested_index=3)
+        cr = self.cr
+        old = cr.research_dataset['total_byte_size']
+        cr.research_dataset['files'] = [file_from_testdata]
+        cr.save()
+        self.assertNotEqual(old, cr.research_dataset['total_byte_size'], 'total_byte_size should be automatically updated if files are changed')
+
+    def test_preservation_state_modified_auto_update(self):
+        cr = self.cr
+        old = cr.preservation_state_modified
+        cr.preservation_state = 1
+        cr.save()
+        self.assertNotEqual(old, cr.preservation_state_modified, 'preservation_state_modified should be automatically updated if changed')
+
+
 class CatalogRecordManagerTests(TestCase, TestClassUtils):
 
     @classmethod
