b96ea8aa4e91d21de48c469342f4813e57e07fa7
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-09-05 13:58:32 +0300

    CSCMETAX-144: [ADD] Generate identifier for DataCatalog on create

--- src/metax_api/tests/api/base/apitests/test_api_data_catalogs.py
+++ src/metax_api/tests/api/base/apitests/test_api_data_catalogs.py
@@ -32,3 +32,27 @@ class DataCatalogApiReadTestV1(APITestCase, TestClassUtils):
         response = self.client.get('/rest/datacatalogs/%s/exists' % 'urn:nbn:fi:non_existing_data_catalog_identifier')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertFalse(response.data)
+
+
+class DataCatalogApiWriteTestV1(APITestCase, TestClassUtils):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Loaded only once for test cases inside this class.
+        """
+        call_command('loaddata', test_data_file_path, verbosity=0)
+        super(DataCatalogApiWriteTestV1, cls).setUpClass()
+
+    def setUp(self):
+        data_catalog_from_test_data = self._get_object_from_test_data('datacatalog', requested_index=0)
+        self.pk = data_catalog_from_test_data['id']
+        self.identifier = data_catalog_from_test_data['catalog_json']['identifier']
+
+    def test_identifier_is_auto_generated(self):
+        from_test_data = self._get_object_from_test_data('datacatalog')
+        from_test_data.pop('id')
+        from_test_data['catalog_json'].pop('identifier')
+        response = self.client.post('/rest/datacatalogs', from_test_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertNotEqual(response.data['catalog_json'].get('identifier', None), None, 'identifier should be created')
