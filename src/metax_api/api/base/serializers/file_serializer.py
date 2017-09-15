5b3a46fa5a65d6db002dfbd6873aab26945d09f0
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-09-15 16:06:11 +0300

    CSCMETAX-167: [ADD] Show checksum-fields as a relation in file-api, instead of model-level fields

--- src/metax_api/api/base/serializers/file_serializer.py
+++ src/metax_api/api/base/serializers/file_serializer.py
@@ -11,6 +11,8 @@ d = logging.getLogger(__name__).debug
 
 class FileSerializer(CommonSerializer):
 
+    _checksum_fields = ['algorithm', 'checked', 'value']
+
     class Meta:
         model = File
         fields = (
@@ -65,6 +67,10 @@ class FileSerializer(CommonSerializer):
                               % type(self.initial_data['file_storage']))
                 raise ValidationError('Validation error for field file_storage. Data in unexpected format')
             self.initial_data['file_storage'] = id
+
+        if 'checksum' in self.initial_data:
+            self._flatten_checksum(self.initial_data['checksum'])
+
         super(FileSerializer, self).is_valid(raise_exception=raise_exception)
 
     def to_representation(self, data):
@@ -73,8 +79,46 @@ class FileSerializer(CommonSerializer):
         # get FileStorage dict from context somehow ?
         fsrs = FileStorageSerializer(FileStorage.objects.get(id=res['file_storage']))
         res['file_storage'] = fsrs.data
+
+        res['checksum'] = self._form_checksum(res)
+        if not res['checksum']:
+            # dont return fields with null values
+            res.pop('checksum')
+
         return res
 
     def validate_file_characteristics(self, value):
         validate_json(value, self.context['view'].json_schema)
         return value
+
+    def _flatten_checksum(self, checksum):
+        """
+        Note: not a real field in the model, but a representation-thing for api users.
+        Flatten values from "relation checksum" to model-level fields:
+
+        'checksum': {
+            'value': val,
+            ...
+        }
+
+        ->
+
+        'checksum_value': val
+
+        """
+        for key in self._checksum_fields:
+            if key in checksum:
+                self.initial_data['checksum_%s' % key] = checksum[key]
+
+    def _form_checksum(self, file_data):
+        """
+        Do the opposite of _flatten_checksum(). Take model-level checksum-fields
+        and form a "relation checksum" to return from the api.
+        """
+        checksum = {}
+        for key in self._checksum_fields:
+            checksum_field = 'checksum_%s' % key
+            if checksum_field in file_data:
+                checksum[key] = file_data[checksum_field]
+                file_data.pop(checksum_field)
+        return checksum
