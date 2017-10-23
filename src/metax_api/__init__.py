9eb908f018510ab2a59acb958da8e6946558b1aa
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-10-23 10:05:38 +0300

    CSCMETAX-61: [REM] Remove celery

--- src/metax_api/__init__.py
+++ src/metax_api/__init__.py
@@ -1,9 +1,1 @@
-from __future__ import absolute_import
-
-# This will make sure celery is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
-
-__all__ = ['celery_app']
-
 default_app_config = 'metax_api.onappstart.OnAppStart'
