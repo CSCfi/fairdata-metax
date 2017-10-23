51b303a20b1b76d5d009409fc0f6b066c7398112
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2017-10-23 09:57:53 +0300

    CSCMETAX-61: [REM] Remove unnecessary middleware from settings.py

--- src/metax_api/settings.py
+++ src/metax_api/settings.py
@@ -65,25 +65,23 @@ else:
 AUTH_USER_MODEL = 'metax_api.MetaxUser'
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'metax_api',
     'rest_framework',
-    'rest_framework_swagger',
 ]
 
+if DEBUG:
+    INSTALLED_APPS.append('django.contrib.staticfiles')
+
 MIDDLEWARE = [
+    # note: not strictly necessary if running in a private network
+    # https://docs.djangoproject.com/en/1.11/ref/middleware/#module-django.middleware.security
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'metax_api.middleware.IdentifyApiCaller',
 ]
 
@@ -104,21 +102,22 @@ ROOT_URLCONF = 'metax_api.urls'
 
 APPEND_SLASH = False
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+if DEBUG:
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
         },
-    },
-]
+    ]
 
 WSGI_APPLICATION = 'metax_api.wsgi.application'
 
@@ -215,24 +214,6 @@ LOGGING = {
 
 logger = logging.getLogger('metax_api')
 logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
