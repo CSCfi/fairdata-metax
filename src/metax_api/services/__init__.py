f36a2ed449f7d2f58c752252b204b6c19f8f3c6e
Joonas Kesäniemi <jkesanie@apro15-gkhtd5.windows.csc.fi>
2018-01-15 14:29:01 +0200

    CSCMETAX-260: [ADD] Read-only Schemas API implementation

--- src/metax_api/services/__init__.py
+++ src/metax_api/services/__init__.py
@@ -3,3 +3,4 @@ from .common_service import CommonService
 from .data_catalog_service import DataCatalogService
 from .file_service import FileService
 from .reference_data_mixin import ReferenceDataMixin
+from .schema_service import SchemaService
