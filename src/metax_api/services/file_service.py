fb96d0a2ed987eb11187f25f0cd9939c99018df7
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2018-09-24 15:32:22 +0300

    CSCMETAX-475: [REF] Refactor get_directory_contents() slightly to simplify the if-else mess

--- src/metax_api/services/file_service.py
+++ src/metax_api/services/file_service.py
@@ -465,72 +465,65 @@ class FileService(CommonService):
         """
         assert request is not None, 'kw parameter request must be specified'
 
-        if identifier and identifier.isdigit() and not include_parent:
-            directory_id = identifier
+        # get targeted directory
+
+        if identifier:
+            try:
+                params = { 'id': int(identifier) }
+            except ValueError:
+                params = { 'identifier': identifier }
         else:
-            if identifier:
-                if identifier.isdigit():
-                    # go this path because parent needs to be included. avoid having to retrieve
-                    # directory info again later
-                    params = { 'id': identifier }
-                else:
-                    params = { 'identifier': identifier }
-            elif path and project_identifier:
+            if path and project_identifier:
                 params = { 'directory_path': path, 'project_identifier': project_identifier }
             else: # pragma: no cover
-                raise ValidationError({'detail': 'no parameters to query by'})
+                raise ValidationError({'detail': ['no parameters to query by']})
 
-            try:
-                # get entire object in case parent has to be included also (include_parent)
-                directory = Directory.objects.get(**params)
-            except Directory.DoesNotExist:
-                raise Http404
+        try:
+            directory = Directory.objects.values('id', 'directory_path', 'project_identifier').get(**params)
+        except Directory.DoesNotExist:
+            raise Http404
 
-            directory_id = directory.id
+        # get cr if relevant
 
         if cr_identifier:
             # browsing in the context of a cr
-            if cr_identifier.isdigit():
+            try:
+                cr_params = { 'id': int(cr_identifier) }
+            except ValueError:
+                cr_params = { 'identifier': cr_identifier }
+
+            try:
                 cr = CatalogRecord.objects.only('id', '_directory_data', 'editor', 'user_created', 'research_dataset').\
-                    get(pk=cr_identifier)
-            else:
-                try:
-                    cr = CatalogRecord.objects.\
-                        only('id', '_directory_data', 'editor', 'user_created', 'research_dataset').\
-                        get(identifier=cr_identifier)
-                except CatalogRecord.DoesNotExist:
-                    # raise 400 instead of 404, to distinguish from the error
-                    # 'directory not found', which raises a 404
-                    raise ValidationError({
-                        'detail': ['CatalogRecord with identifier %s does not exist' % cr_identifier]
-                    })
+                    get(**cr_params)
+            except CatalogRecord.DoesNotExist:
+                # raise 400 instead of 404, to distinguish from the error
+                # 'directory not found', which raises a 404
+                raise ValidationError({
+                    'detail': [ 'CatalogRecord with identifier %s does not exist' % cr_identifier ]
+                })
 
             if not cr.authorized_to_see_catalog_record_files(request):
                 raise Http403({
-                    'detail': ['You do not have permission to see this information because the dataset access type is '
-                               'not open and you are not the owner of the catalog record.']
+                    'detail': [
+                        'You do not have permission to see this information because the dataset access type is '
+                        'not open and you are not the owner of the catalog record.'
+                    ]
                 })
+
             cr_id = cr.id
             cr_directory_data = cr._directory_data or {}
         else:
             # generally browsing the directory - NOT in the context of a cr! check user permissions
             if not request.user.is_service:
-                if not project_identifier:
-                    try:
-                        project_identifier = directory.project_identifier
-                    except:
-                        project_identifier = Directory.objects.filter(pk=directory_id) \
-                            .values_list('project_identifier', flat=True)[0]
-                FileService.check_user_belongs_to_project(request, project_identifier)
-
+                FileService.check_user_belongs_to_project(request, directory['project_identifier'])
             cr_id = None
             cr_directory_data = {}
 
-        # note: by default all fields are retrieved
+        # get list of field names to retrieve. note: by default all fields are retrieved
         directory_fields, file_fields = cls._get_requested_file_browsing_fields(request)
 
         contents = cls._get_directory_contents(
-            directory_id,
+            directory['id'],
             recursive=recursive,
             max_depth=max_depth,
             dirs_only=dirs_only,
@@ -552,7 +545,8 @@ class FileService(CommonService):
                 return file_list
 
         if include_parent:
-            contents.update(DirectorySerializer(directory, only_fields=directory_fields).data)
+            from metax_api.api.rest.base.serializers import LightDirectorySerializer
+            contents.update(LightDirectorySerializer.serialize(directory))
 
         if cls._include_total_byte_sizes_and_file_counts(cr_id, directory_fields):
             cls.retrieve_directory_byte_sizes_and_file_counts_for_cr(contents, cr_id,
