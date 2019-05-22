87425eddb1dc01718b638bc913d19d83b8ead574
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2019-05-22 15:18:31 +0300

    CSCMETAX-552: [ADD] Make API permissions more fine grained, by adding read, create, update, delete permissions per service or user group (end users), instead of only read or write

--- src/metax_api/tests/api/rest/base/views/files/write.py
+++ src/metax_api/tests/api/rest/base/views/files/write.py
@@ -1119,19 +1119,58 @@ class FileApiWriteEndUserAccess(FileApiWriteCommon):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     @responses.activate
-    def test_user_cant_update_files(self):
+    def test_user_can_only_update_permitted_file_fields(self):
         '''
-        Ensure users are unable to modify existing files.
+        Ensure users are only able to modify permitted fields.
         '''
-
         # ensure user belongs to same project
         proj = File.objects.get(pk=1).project_identifier
         self.token['group_names'].append('fairdata:IDA01:%s' % proj)
         self._use_http_authorization(method='bearer', token=self.token)
 
         response = self.client.get('/rest/files/1', format="json")
-        response = self.client.put('/rest/files/1', response.data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN, response.data)
+        file = response.data
+        original_file = deepcopy(file)
+        file['byte_size'] = 200
+        file['checksum']['value'] = 'changed'
+        file['parent_directory'] = 1
+        file['file_frozen'] = '3' + file['file_frozen'][1:]
+        file['file_format'] = 'changed'
+        file['file_name'] = 'changed'
+        file['file_path'] = '/oh/no'
+        file['file_storage'] = 2
+        file['file_uploaded'] = '3' + file['file_uploaded'][1:]
+        file['identifier'] = 'changed'
+        file['open_access'] = True
+        file['project_identifier'] = 'changed'
+        file['service_modified'] = 'changed'
+        file['service_created'] = 'changed'
+        file['removed'] = True
+
+        # the only field that should be changed
+        file['file_characteristics'] = { 'title': 'new title'}
+
+        response = self.client.put('/rest/files/1', file, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.data['file_characteristics']['title'], 'new title', response.data)
+
+        for key, value in response.data.items():
+            try:
+                if key in ('date_modified', 'file_modified'):
+                    # these fields are changed by metax
+                    continue
+                elif key == 'file_characteristics':
+                    # the field that should have been changed by the user
+                    self.assertNotEqual(original_file[key], response.data[key])
+                else:
+                    # must not have changed
+                    self.assertEqual(original_file[key], response.data[key])
+            except KeyError as e:
+                if e.args[0] == 'user_modified':
+                    # added by metax
+                    continue
+                raise
+
 
 class FileApiWriteDryrunTest(FileApiWriteCommon):
 
