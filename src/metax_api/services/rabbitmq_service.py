8e4c974d1877b2b64baba39190904dee7e556eaa
Evgenia Lyjina <jenny.lyjina@gmail.com>
2020-02-19 13:02:53 +0200

    CSCFAIRMETA-470:[REF] clean out printing.

--- src/metax_api/services/rabbitmq_service.py
+++ src/metax_api/services/rabbitmq_service.py
@@ -88,7 +88,6 @@ class _RabbitMQService():
                     message = json_dumps(
                         message,
                         cls=DjangoJSONEncoder)
-                    print(message)
                 self._channel.basic_publish(body=message, routing_key=routing_key, exchange=exchange, **additional_args)
         except Exception as e:
             _logger.error(e)
