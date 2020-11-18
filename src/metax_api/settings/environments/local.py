92986bd89c0ec9ae1042ec31dfac394715dfb90c
Toni Nurmi <toni.nurmi@csc.fi>
2020-11-18 13:10:49 +0200

    Allow all hosts if environment is local

--- src/metax_api/settings/environments/local.py
+++ src/metax_api/settings/environments/local.py
@@ -1,6 +1,8 @@
-from metax_api.settings.components.common import INSTALLED_APPS
+from metax_api.settings.components.common import INSTALLED_APPS, ALLOWED_HOSTS
 from watchman import constants as watchman_constants
 INSTALLED_APPS += ['watchman']
+
+ALLOWED_HOSTS += ["*"]
 
 WATCHMAN_CHECKS = watchman_constants.DEFAULT_CHECKS + (
     'metax_api.checks.elasticsearch_check',
