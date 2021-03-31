ef8fdafa0953b3408a174660f717517265dd5067
Tommi Pulli <tommi.pulli7@gmail.com>
2021-03-31 14:00:54 +0300

    fix TTV load data script

--- src/metax_api/management/commands/load_data_to_TTV.py
+++ src/metax_api/management/commands/load_data_to_TTV.py
@@ -6,31 +6,42 @@
 # :license: MIT
 
 import logging
-import json
 
 from django.core.management.base import BaseCommand
 from metax_api.models import CatalogRecord
 from metax_api.services import RabbitMQService
-from metax_api.api.rest.base.serializers import CatalogRecordSerializer
 
 logger = logging.getLogger(__name__)
 
+# serializer needs these in context so give them.
+# definitely not the way to do it but still, here it is..
+class User:
+    def __init__(self):
+        self.is_service = True
+
+class Request:
+    def __init__(self, user):
+        self.user = user
+        self.query_params = []
+        self.method = 'POST'
 
 class Command(BaseCommand):
 
-	help = "Upload all existing data to TTV's RabbitMQ queue"
+    help = "Upload all existing data to TTV's RabbitMQ queue"
 
-	def handle(self, *args, **options):
-		catalog_records = CatalogRecord.objects.all()
-		logger.info(f"found {catalog_records.count()} catalog records")
-		aff_rows = 0
+    def handle(self, *args, **options):
+        catalog_records = CatalogRecord.objects.filter(state='published')
+        aff_rows = 0
+        user = User()
+        request = Request(user)
+        context = {'request': request}
 
-		for catalog_record in catalog_records:
-			cr = CatalogRecordSerializer(catalog_record).data
-			logger.info(f"Publishing catalog record {aff_rows} from TOTAL {catalog_records.count()}")
-			RabbitMQService.publish(cr, routing_key='', exchange="TTV-datasets")
-			aff_rows += 1
-			logger.info(f"Published catalog record id={cr['identifier']}")
+        for catalog_record in catalog_records:
+            serializer = catalog_record.serializer_class
+            cr_json = serializer(catalog_record, context=context).data
+            cr_json['data_catalog'] = {'catalog_json': catalog_record.data_catalog.catalog_json}
 
-		logger.info(f"All catalog records published")
+            RabbitMQService.publish(cr_json, routing_key='create', exchange="TTV-datasets")
+            aff_rows += 1
 
+        logger.info("All catalog records published to TTV exchange")
