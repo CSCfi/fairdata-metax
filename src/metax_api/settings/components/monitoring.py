7c29b124c477d1056fe0acf000ab0e25e20b40dc
Toni Nurmi <tonurmi@csc.fi>
2021-04-14 12:04:45 +0300

    add monitoring component module to settings

---
+++ src/metax_api/settings/components/monitoring.py
@@ -0,0 +1,19 @@
+from metax_api.settings import env
+from metax_api.settings.components.common import INSTALLED_APPS
+import logging
+
+logger = logging.getLogger(__name__)
+ENABLE_DJANGO_WATCHMAN = env("ENABLE_DJANGO_WATCHMAN")
+WATCHMAN_CONFIGURED = False
+if ENABLE_DJANGO_WATCHMAN:
+    try:
+        from watchman import constants as watchman_constants
+        if "watchman" not in INSTALLED_APPS:
+            INSTALLED_APPS += ["watchman"]
+            WATCHMAN_CHECKS = watchman_constants.DEFAULT_CHECKS + (
+                "metax_api.checks.elasticsearch_check",
+                "metax_api.checks.redis_check",
+            )
+            WATCHMAN_CONFIGURED = True
+    except ImportError as e:
+        logger.error(e)
