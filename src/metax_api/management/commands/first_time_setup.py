d5105805380269329dc734477b165ea7011fb3fc
Toni Nurmi <tonurmi@csc.fi>
2021-04-26 14:05:55 +0300

    implement first_time_setup management command

---
+++ src/metax_api/management/commands/first_time_setup.py
@@ -0,0 +1,15 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        call_command("index_refdata")
+        call_command("reload_refdata_cache")
+        call_command("loadinitialdata")
+        call_command("loaddata" "metax_api/tests/testdata/test_data.json")
+        logger.info("All first time setup commands completed successfully")
