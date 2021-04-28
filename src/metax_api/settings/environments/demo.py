04a575ce5913291fb9cafbc51962774b94803902
Toni Nurmi <tonurmi@csc.fi>
2021-04-28 12:47:24 +0300

    add demo env, clear flush endpoint perms only in production env

---
+++ src/metax_api/settings/environments/demo.py
@@ -0,0 +1,17 @@
+
+from metax_api.settings.components.access_control import Role, api_permissions, prepare_perm_values
+
+api_permissions.rest.datasets.create += [Role.IDA, Role.QVAIN_LIGHT, Role.JYU]
+api_permissions.rest.datasets["update"] += [Role.IDA, Role.QVAIN_LIGHT, Role.JYU]
+api_permissions.rest.datasets.delete += [Role.IDA, Role.QVAIN_LIGHT, Role.JYU]
+
+api_permissions.rest.directories.read += [Role.IDA, Role.QVAIN_LIGHT]
+
+api_permissions.rest.files.read += [Role.QVAIN, Role.QVAIN_LIGHT]
+api_permissions.rest.files["update"] += [Role.QVAIN, Role.QVAIN_LIGHT]
+
+api_permissions.rpc.datasets.change_cumulative_state.use = [Role.METAX, Role.QVAIN, Role.QVAIN_LIGHT, Role.END_USER]
+api_permissions.rpc.datasets.fix_deprecated.use = [Role.METAX, Role.QVAIN, Role.QVAIN_LIGHT, Role.END_USER]
+api_permissions.rpc.dataset.refresh_directory_content.use = [Role.METAX, Role.QVAIN, Role.QVAIN_LIGHT, Role.END_USER]
+
+API_ACCESS = prepare_perm_values(api_permissions)
