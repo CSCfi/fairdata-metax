0eab45c51cfa1d8cc65bf6a462f93be0f1813f75
aptiaine <ari-pekka.tiainen@csc.fi>
2021-06-09 15:59:52 +0300

    CSCFAIRMETA-1056 move swagger to Metax Api

--- src/metax_api/utils/convert_yaml_to_html.py
+++ src/metax_api/utils/convert_yaml_to_html.py
@@ -4,6 +4,8 @@ import yaml
 from os import path
 from django.conf import settings
 
+_logger = logging.getLogger(__name__)
+
 TEMPLATE = """
 <!DOCTYPE html>
 <html lang="en">
@@ -66,25 +68,51 @@ window.onload = function() {
 </html>
 """
 
+FAIL_TEMPLATE = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Swagger file creation failed</title>
+</head>
+<body>
+    <h2>Swagger file creation failed!</h2>
+</body>
+</html>
+"""
 
 def yaml_to_html_convert():
-    inpathv1 = path.join(settings.SWAGGER_YAML_PATH, 'v1', 'swagger.yaml')
-    inpathv2 = path.join(settings.SWAGGER_YAML_PATH, 'v2', 'swagger.yaml')
-    outpathv1 = path.join(settings.SWAGGER_HTML_PATH, 'v1', 'swagger.html')
-    outpathv2 = path.join(settings.SWAGGER_HTML_PATH, 'v2', 'swagger.html')
-    infile1 = open(inpathv1, 'r')
-    outfile1 = open(outpathv1, 'w')
-    spec = yaml.load(infile1, Loader=yaml.FullLoader)
-    if spec.get("host") =="__METAX_ENV_DOMAIN__":
-        spec["host"] = settings.SERVER_DOMAIN_NAME
-    outfile1.write(TEMPLATE % json.dumps(spec))
-    infile1.close()
-    outfile1.close()
-    infile2 = open(inpathv2, 'r')
-    outfile2 = open(outpathv2, 'w')
-    spec = yaml.load(infile2, Loader=yaml.FullLoader)
-    if spec.get("host") =="__METAX_ENV_DOMAIN__":
-        spec["host"] = settings.SERVER_DOMAIN_NAME
-    outfile2.write(TEMPLATE % json.dumps(spec))
-    infile2.close()
-    outfile2.close()
+    try:
+        inpathv1 = path.join(settings.SWAGGER_YAML_PATH, 'v1', 'swagger.yaml')
+        inpathv2 = path.join(settings.SWAGGER_YAML_PATH, 'v2', 'swagger.yaml')
+        outpathv1 = path.join(settings.SWAGGER_HTML_PATH, 'v1', 'swagger.html')
+        outpathv2 = path.join(settings.SWAGGER_HTML_PATH, 'v2', 'swagger.html')
+        outfile1 = open(outpathv1, 'w')
+        outfile2 = open(outpathv2, 'w')
+        try:
+            infile1 = open(inpathv1, 'r')
+            spec = yaml.load(infile1, Loader=yaml.FullLoader)
+            if spec.get("host") =="__METAX_ENV_DOMAIN__":
+                spec["host"] = settings.SERVER_DOMAIN_NAME
+            outfile1.write(TEMPLATE % json.dumps(spec))
+            infile1.close()
+        except FileNotFoundError:
+            outfile1.write(FAIL_TEMPLATE)
+
+        try:
+            infile2 = open(inpathv2, 'r')
+            spec = yaml.load(infile2, Loader=yaml.FullLoader)
+            if spec.get("host") =="__METAX_ENV_DOMAIN__":
+                spec["host"] = settings.SERVER_DOMAIN_NAME
+            outfile2.write(TEMPLATE % json.dumps(spec))
+            infile2.close()
+        except FileNotFoundError:
+            outfile2.write(FAIL_TEMPLATE)
+        outfile1.close()
+        outfile2.close()
+    except yaml.YAMLError as exc:
+        _logger.error("YAML loading failed")
+        _logger.error(exc)
+    except json.decoder.JSONDecodeError as exc:
+        _logger.error("JSON loading failed")
+        _logger.error(exc)
