52acf7def57f7c1a40fef61c166dd4895c1476d3
aptiaine <ari-pekka.tiainen@csc.fi>
2021-06-15 15:20:14 +0300

    Added more error handling and removed duplicate code block

--- src/metax_api/utils/convert_yaml_to_html.py
+++ src/metax_api/utils/convert_yaml_to_html.py
@@ -1,7 +1,7 @@
 import logging
 import json
 import yaml
-from os import path
+from os import path, makedirs
 from django.conf import settings
 
 _logger = logging.getLogger(__name__)
@@ -81,38 +81,38 @@ FAIL_TEMPLATE = """
 </html>
 """
 
-def yaml_to_html_convert():
-    try:
-        inpathv1 = path.join(settings.SWAGGER_YAML_PATH, 'v1', 'swagger.yaml')
-        inpathv2 = path.join(settings.SWAGGER_YAML_PATH, 'v2', 'swagger.yaml')
-        outpathv1 = path.join(settings.SWAGGER_HTML_PATH, 'v1', 'swagger.html')
-        outpathv2 = path.join(settings.SWAGGER_HTML_PATH, 'v2', 'swagger.html')
-        outfile1 = open(outpathv1, 'w')
-        outfile2 = open(outpathv2, 'w')
-        try:
-            infile1 = open(inpathv1, 'r')
-            spec = yaml.load(infile1, Loader=yaml.FullLoader)
-            if spec.get("host") =="__METAX_ENV_DOMAIN__":
-                spec["host"] = settings.SERVER_DOMAIN_NAME
-            outfile1.write(TEMPLATE % json.dumps(spec))
-            infile1.close()
-        except FileNotFoundError:
-            outfile1.write(FAIL_TEMPLATE)
 
-        try:
-            infile2 = open(inpathv2, 'r')
-            spec = yaml.load(infile2, Loader=yaml.FullLoader)
-            if spec.get("host") =="__METAX_ENV_DOMAIN__":
-                spec["host"] = settings.SERVER_DOMAIN_NAME
-            outfile2.write(TEMPLATE % json.dumps(spec))
-            infile2.close()
-        except FileNotFoundError:
-            outfile2.write(FAIL_TEMPLATE)
-        outfile1.close()
-        outfile2.close()
-    except yaml.YAMLError as exc:
-        _logger.error("YAML loading failed")
-        _logger.error(exc)
-    except json.decoder.JSONDecodeError as exc:
-        _logger.error("JSON loading failed")
-        _logger.error(exc)
+def yaml_to_html_convert():
+
+    try:
+        for api_version in ["v1", "v2"]:
+            inpath = path.join(settings.SWAGGER_YAML_PATH, api_version, 'swagger.yaml')
+            outpath = path.join(settings.SWAGGER_HTML_PATH, api_version, 'swagger.html')
+
+            if not path.exists(path.dirname(outpath)):
+                try:
+                    makedirs(path.dirname(outpath))
+                except OSError as exc:
+                    _logger.error(exc)
+                    raise
+
+            with open(outpath, 'w') as outfile:
+                try:
+                    with open(inpath, 'r') as infile:
+                        spec = yaml.load(infile, Loader=yaml.FullLoader)
+                        if spec.get("host") == "__METAX_ENV_DOMAIN__":
+                            spec["host"] = settings.SERVER_DOMAIN_NAME
+                        outfile.write(TEMPLATE % json.dumps(spec))
+
+                except FileNotFoundError:
+                    outfile.write(FAIL_TEMPLATE)
+                except yaml.YAMLError as exc:
+                    _logger.error("YAML loading failed")
+                    _logger.error(exc)
+                    outfile.write(FAIL_TEMPLATE)
+                except json.decoder.JSONDecodeError as exc:
+                    _logger.error("JSON loading failed")
+                    _logger.error(exc)
+
+    except PermissionError:
+        _logger.error("Permission error")
