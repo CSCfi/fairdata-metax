83ae5bb6ae74f511b12eda965fddec6e79be8fad
Sini Saarinen <ssaarine@h820g3l-261l9c.x.csc.fi>
2021-07-21 10:46:50 +0300

    removed duplicate tests from v2 tests

--- src/metax_api/tests/api/rest/v2/views/datacatalogs/write.py
+++ src/metax_api/tests/api/rest/v2/views/datacatalogs/write.py
@@ -9,8 +9,6 @@ from django.core.management import call_command
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from metax_api.models import DataCatalog
-from metax_api.services.redis_cache_service import RedisClient
 from metax_api.tests.utils import TestClassUtils, test_data_file_path
 
 
@@ -31,14 +29,6 @@ class DataCatalogApiWriteCommon(APITestCase, TestClassUtils):
 
 
 class DataCatalogApiWriteBasicTests(DataCatalogApiWriteCommon):
-    def test_identifier_is_auto_generated(self):
-        response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertNotEqual(
-            response.data["catalog_json"].get("identifier", None),
-            None,
-            "identifier should be created",
-        )
 
     def test_research_dataset_schema_missing_ok(self):
         self.new_test_data["catalog_json"].pop("research_dataset_schema", None)
@@ -56,141 +46,3 @@ class DataCatalogApiWriteBasicTests(DataCatalogApiWriteCommon):
         response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual("versioning" in response.data["detail"][0], True, response.data)
-
-    def test_create_identifier_already_exists(self):
-        response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            "already exists" in response.data["catalog_json"]["identifier"][0],
-            True,
-            response.data,
-        )
-
-    def test_delete(self):
-        response = self.client.delete("/rest/v2/datacatalogs/1")
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-        dc_deleted = DataCatalog.objects_unfiltered.get(pk=1)
-        self.assertEqual(dc_deleted.removed, True)
-        self.assertEqual(
-            dc_deleted.date_modified,
-            dc_deleted.date_removed,
-            "date_modified should be updated",
-        )
-
-
-class DataCatalogApiWriteReferenceDataTests(DataCatalogApiWriteCommon):
-    """
-    Tests related to reference_data validation and data catalog fields population
-    from reference_data, according to given uri or code as the value.
-    """
-
-    def test_create_data_catalog_with_invalid_reference_data(self):
-        dc = self.new_test_data["catalog_json"]
-        dc["field_of_science"][0]["identifier"] = "nonexisting"
-        dc["language"][0]["identifier"] = "nonexisting"
-        dc["access_rights"]["access_type"][0]["identifier"] = "nonexisting"
-        dc["access_rights"]["license"][0]["identifier"] = "nonexisting"
-        response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual("catalog_json" in response.data.keys(), True)
-        self.assertEqual(len(response.data["catalog_json"]), 4)
-
-    def test_create_data_catalog_populate_fields_from_reference_data(self):
-        """
-        1) Insert codes from cached reference data to data catalog identifier fields
-           that will be validated, and then populated
-        2) Check that that the values in data catalog identifier fields are changed from
-           codes to uris after a successful create
-        3) Check that labels have also been copied to data catalog to their approriate fields
-        """
-        cache = RedisClient()
-        refdata = cache.get("reference_data")["reference_data"]
-        orgdata = cache.get("reference_data")["organization_data"]
-        refs = {}
-
-        data_types = [
-            "access_type",
-            "field_of_science",
-            "language",
-            "license",
-        ]
-
-        # the values in these selected entries will be used throghout the rest of the test case
-        for dtype in data_types:
-            entry = refdata[dtype][0]
-            refs[dtype] = {
-                "code": entry["code"],
-                "uri": entry["uri"],
-                "label": entry.get("label", None),
-            }
-
-        refs["organization"] = {
-            "uri": orgdata["organization"][1]["uri"],
-            "code": orgdata["organization"][1]["code"],
-            "label": orgdata["organization"][1]["label"],
-        }
-
-        # replace the relations with objects that have only the identifier set with code as value,
-        # to easily check that label was populated (= that it appeared in the dataset after create)
-        # without knowing its original value from the generated test data
-        dc = self.new_test_data["catalog_json"]
-        dc["field_of_science"][0] = {"identifier": refs["field_of_science"]["code"]}
-        dc["language"][0] = {"identifier": refs["language"]["code"]}
-        dc["access_rights"]["access_type"][0] = {"identifier": refs["access_type"]["code"]}
-        dc["access_rights"]["license"][0] = {"identifier": refs["license"]["code"]}
-
-        # these have other required fields, so only update the identifier with code
-        dc["publisher"]["identifier"] = refs["organization"]["code"]
-        dc["access_rights"]["has_rights_related_agent"][0]["identifier"] = refs["organization"][
-            "code"
-        ]
-
-        response = self.client.post("/rest/v2/datacatalogs", self.new_test_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
-        self.assertEqual("catalog_json" in response.data.keys(), True)
-
-        new_dc = response.data["catalog_json"]
-        self._assert_uri_copied_to_identifier(refs, new_dc)
-        self._assert_label_copied_to_pref_label(refs, new_dc)
-        self._assert_label_copied_to_title(refs, new_dc)
-        self._assert_label_copied_to_name(refs, new_dc)
-
-    def _assert_uri_copied_to_identifier(self, refs, new_dc):
-        self.assertEqual(
-            refs["field_of_science"]["uri"], new_dc["field_of_science"][0]["identifier"]
-        )
-        self.assertEqual(refs["language"]["uri"], new_dc["language"][0]["identifier"])
-        self.assertEqual(
-            refs["access_type"]["uri"],
-            new_dc["access_rights"]["access_type"][0]["identifier"],
-        )
-        self.assertEqual(
-            refs["license"]["uri"], new_dc["access_rights"]["license"][0]["identifier"]
-        )
-        self.assertEqual(refs["organization"]["uri"], new_dc["publisher"]["identifier"])
-        self.assertEqual(
-            refs["organization"]["uri"],
-            new_dc["access_rights"]["has_rights_related_agent"][0]["identifier"],
-        )
-
-    def _assert_label_copied_to_pref_label(self, refs, new_dc):
-        self.assertEqual(
-            refs["field_of_science"]["label"],
-            new_dc["field_of_science"][0].get("pref_label", None),
-        )
-        self.assertEqual(
-            refs["access_type"]["label"],
-            new_dc["access_rights"]["access_type"][0].get("pref_label", None),
-        )
-
-    def _assert_label_copied_to_title(self, refs, new_dc):
-        self.assertEqual(
-            refs["license"]["label"],
-            new_dc["access_rights"]["license"][0].get("title", None),
-        )
-
-    def _assert_label_copied_to_name(self, refs, new_dc):
-        self.assertEqual(refs["organization"]["label"], new_dc["publisher"]["name"])
--- src/metax_api/tests/api/rpc/v2/views/dataset_rpc.py
+++ src/metax_api/tests/api/rpc/v2/views/dataset_rpc.py
@@ -7,119 +7,13 @@
 
 import responses
 from django.conf import settings
-from django.core.management import call_command
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 from metax_api.models import CatalogRecordV2
 from metax_api.tests.api.rest.base.views.datasets.write import CatalogRecordApiWriteCommon
-from metax_api.tests.utils import TestClassUtils, get_test_oidc_token, test_data_file_path
+from metax_api.tests.utils import get_test_oidc_token
 
 CR = CatalogRecordV2
-
-
-class DatasetRPCTests(APITestCase, TestClassUtils):
-    @classmethod
-    def setUpClass(cls):
-        """
-        Loaded only once for test cases inside this class.
-        """
-        super().setUpClass()
-        call_command("loaddata", test_data_file_path, verbosity=0)
-
-    def setUp(self):
-        super().setUp()
-        self.create_end_user_data_catalogs()
-
-    @responses.activate
-    def test_get_minimal_dataset_template(self):
-        """
-        Retrieve and use a minimal dataset template example from the api.
-        """
-
-        # query param type is missing, should return error and description what to do.
-        response = self.client.get("/rpc/v2/datasets/get_minimal_dataset_template")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        # test preventing typos
-        response = self.client.get("/rpc/v2/datasets/get_minimal_dataset_template?type=wrong")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        # test minimal dataset for service use
-        response = self.client.get("/rpc/v2/datasets/get_minimal_dataset_template?type=service")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue("metadata_provider_org" in response.data)
-        self.assertTrue("metadata_provider_user" in response.data)
-        self._use_http_authorization(username="testuser")
-        response = self.client.post("/rest/v2/datasets", response.data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        # test minimal dataset for end user use
-        response = self.client.get("/rpc/v2/datasets/get_minimal_dataset_template?type=enduser")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue("metadata_provider_org" not in response.data)
-        self.assertTrue("metadata_provider_user" not in response.data)
-        self._use_http_authorization(method="bearer", token=get_test_oidc_token())
-        self._mock_token_validation_succeeds()
-        response = self.client.post("/rest/v2/datasets", response.data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_set_preservation_identifier(self):
-        self._set_http_authorization("service")
-
-        # Parameter 'identifier' is required
-        response = self.client.post("/rpc/v2/datasets/set_preservation_identifier")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        # Nonexisting identifier should return 404
-        response = self.client.post(
-            "/rpc/v2/datasets/set_preservation_identifier?identifier=nonexisting"
-        )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-        # Create ida data catalog
-        dc = self._get_object_from_test_data("datacatalog", requested_index=0)
-        dc_id = settings.IDA_DATA_CATALOG_IDENTIFIER
-        dc["catalog_json"]["identifier"] = dc_id
-        self.client.post("/rest/v2/datacatalogs", dc, format="json")
-
-        # Test OK ops
-
-        # Create new ida cr without doi
-        cr_json = self.client.get("/rest/v2/datasets/1").data
-        cr_json.pop("preservation_identifier", None)
-        cr_json.pop("identifier")
-        cr_json["research_dataset"].pop("preferred_identifier", None)
-        cr_json["data_catalog"] = dc_id
-        cr_json["research_dataset"]["issued"] = "2018-01-01"
-        cr_json["research_dataset"]["publisher"] = {
-            "@type": "Organization",
-            "name": {"en": "publisher"},
-        }
-
-        response = self.client.post("/rest/v2/datasets?pid_type=urn", cr_json, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
-        identifier = response.data["identifier"]
-
-        # Verify rpc api returns the same doi as the one that is set to the datasets' preservation identifier
-        response = self.client.post(
-            f"/rpc/v2/datasets/set_preservation_identifier?identifier={identifier}"
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
-
-        response2 = self.client.get(f"/rest/v2/datasets/{identifier}")
-        self.assertEqual(response2.status_code, status.HTTP_200_OK, response2.data)
-        self.assertEqual(response.data, response2.data["preservation_identifier"], response2.data)
-
-        # Return 400 if request is not correct datacite format
-        response2.data["research_dataset"].pop("issued")
-        response = self.client.put(f"/rest/v2/datasets/{identifier}", response2.data, format="json")
-        self.assertEqual(response2.status_code, status.HTTP_200_OK, response2.data)
-
-        response = self.client.post(
-            f"/rpc/v2/datasets/set_preservation_identifier?identifier={identifier}"
-        )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
 
 
 class ChangeCumulativeStateRPC(CatalogRecordApiWriteCommon):
