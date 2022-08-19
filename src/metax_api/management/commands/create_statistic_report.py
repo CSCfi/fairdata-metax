0f46f8e83e554a134fd8f7d3ef6409691b3b632d
Atro Himanen <atrohim@palokarki.lnx.csc.fi>
2022-08-19 14:55:04 +0300

    CSCFAIRMETA-1539: Ran Black formatting

--- src/metax_api/management/commands/create_statistic_report.py
+++ src/metax_api/management/commands/create_statistic_report.py
@@ -4,60 +4,90 @@ from django.core.management.base import BaseCommand
 from django.db.models import Sum
 from django.db.models.expressions import RawSQL
 
-from metax_api.models import File, CatalogRecordV2, OrganizationStatistics, ProjectStatistics, FileStorage
+from metax_api.models import (
+    File,
+    CatalogRecordV2,
+    OrganizationStatistics,
+    ProjectStatistics,
+    FileStorage,
+)
 from metax_api.api.rest.base.views import FileViewSet
 from metax_api.services import FileService, StatisticService
 
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		
-		logger.info("Creating statistic summary")
+    def handle(self, *args, **options):
 
-		OrganizationStatistics.objects.all().delete()
-		ProjectStatistics.objects.all().delete()
+        logger.info("Creating statistic summary")
 
-		ida_file_storage = FileStorage.objects.get(file_storage_json__icontains="urn:nbn:fi:att:file-storage-ida")
-		pas_file_storage = FileStorage.objects.get(file_storage_json__icontains="urn:nbn:fi:att:file-storage-pas")
+        OrganizationStatistics.objects.all().delete()
+        ProjectStatistics.objects.all().delete()
 
-		all_projects = File.objects.all().values("project_identifier").distinct()
+        ida_file_storage = FileStorage.objects.get(
+            file_storage_json__icontains="urn:nbn:fi:att:file-storage-ida"
+        )
+        pas_file_storage = FileStorage.objects.get(
+            file_storage_json__icontains="urn:nbn:fi:att:file-storage-pas"
+        )
 
-		for project in all_projects:
-			logger.info(f"all_project: {project}")
-			project_id = project["project_identifier"]
+        all_projects = File.objects.all().values("project_identifier").distinct()
 
-			ida_files_stats = StatisticService.count_files([project_id], removed="false", include_pids=True, file_storage=ida_file_storage)
-			ida_count = ida_files_stats[0]["count"]
-			ida_size = ida_files_stats[0]["byte_size"]
-			ida_file_pids = ida_files_stats[1]
-			ida_published_catalog_record_pids = self.get_published_cr_pids(ida_file_pids)
+        for project in all_projects:
+            logger.info(f"all_project: {project}")
+            project_id = project["project_identifier"]
 
-			pas_files_stats = StatisticService.count_files([project_id], removed="false", include_pids=True, file_storage=pas_file_storage)
-			pas_count = pas_files_stats[0]["count"]
-			pas_size = pas_files_stats[0]["byte_size"]
-			pas_file_pids = pas_files_stats[1]
-			pas_published_catalog_record_pids = self.get_published_cr_pids(pas_file_pids)
+            ida_files_stats = StatisticService.count_files(
+                [project_id], removed="false", include_pids=True, file_storage=ida_file_storage
+            )
+            ida_count = ida_files_stats[0]["count"]
+            ida_size = ida_files_stats[0]["byte_size"]
+            ida_file_pids = ida_files_stats[1]
+            ida_published_catalog_record_pids = self.get_published_cr_pids(ida_file_pids)
 
-			stat = ProjectStatistics(project_id, ida_count, ida_size, ida_published_catalog_record_pids, pas_count, pas_size, pas_published_catalog_record_pids)
-			stat.save()
+            pas_files_stats = StatisticService.count_files(
+                [project_id], removed="false", include_pids=True, file_storage=pas_file_storage
+            )
+            pas_count = pas_files_stats[0]["count"]
+            pas_size = pas_files_stats[0]["byte_size"]
+            pas_file_pids = pas_files_stats[1]
+            pas_published_catalog_record_pids = self.get_published_cr_pids(pas_file_pids)
 
+            stat = ProjectStatistics(
+                project_id,
+                ida_count,
+                ida_size,
+                ida_published_catalog_record_pids,
+                pas_count,
+                pas_size,
+                pas_published_catalog_record_pids,
+            )
+            stat.save()
 
-		organizations = CatalogRecordV2.objects.all().order_by().values("metadata_provider_org").distinct()
+        organizations = (
+            CatalogRecordV2.objects.all().order_by().values("metadata_provider_org").distinct()
+        )
 
-		for org in organizations:
-			org_id = org["metadata_provider_org"]
-			ret = StatisticService.count_datasets(metadata_provider_org=org_id)
-			stat = OrganizationStatistics(org_id, ret["count"], ret["ida_byte_size"])
-			stat.save()
+        for org in organizations:
+            org_id = org["metadata_provider_org"]
+            ret = StatisticService.count_datasets(metadata_provider_org=org_id)
+            stat = OrganizationStatistics(org_id, ret["count"], ret["ida_byte_size"])
+            stat.save()
 
-		logger.info("Statistic summary created")
+        logger.info("Statistic summary created")
 
-	def get_published_cr_pids(self, file_pids):
+    def get_published_cr_pids(self, file_pids):
 
-		if len(file_pids) == 0:
-			return ""
-		else:
-			all_catalog_records = FileService.get_identifiers(file_pids, "noparams", True).data
-			published_catalog_records = CatalogRecordV2.objects.filter(identifier__in = all_catalog_records, state = "published")
-			return list(published_catalog_records.values_list('research_dataset__preferred_identifier', flat = True).distinct())
+        if len(file_pids) == 0:
+            return ""
+        else:
+            all_catalog_records = FileService.get_identifiers(file_pids, "noparams", True).data
+            published_catalog_records = CatalogRecordV2.objects.filter(
+                identifier__in=all_catalog_records, state="published"
+            )
+            return list(
+                published_catalog_records.values_list(
+                    "research_dataset__preferred_identifier", flat=True
+                ).distinct()
+            )
--- src/metax_api/models/project_statistics.py
+++ src/metax_api/models/project_statistics.py
@@ -11,11 +11,12 @@ from django.db import models
 
 _logger = logging.getLogger(__name__)
 
+
 class ProjectStatistics(models.Model):
-	project_identifier = models.CharField(primary_key=True, max_length=200)
-	ida_count = models.IntegerField()
-	ida_byte_size = models.BigIntegerField()
-	ida_published_datasets = models.TextField()
-	pas_count = models.IntegerField(default=0)
-	pas_byte_size = models.BigIntegerField(default=0)
-	pas_published_datasets = models.TextField(default="")
+    project_identifier = models.CharField(primary_key=True, max_length=200)
+    ida_count = models.IntegerField()
+    ida_byte_size = models.BigIntegerField()
+    ida_published_datasets = models.TextField()
+    pas_count = models.IntegerField(default=0)
+    pas_byte_size = models.BigIntegerField(default=0)
+    pas_published_datasets = models.TextField(default="")
