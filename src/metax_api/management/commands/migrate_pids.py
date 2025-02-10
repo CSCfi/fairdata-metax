4f3483c3b46d21421fc286a622a21c7579298fbb
Atro Himanen <atrohim@csc.fi>
2025-02-10 06:13:53 +0000

    Added --allow-fail flag to migrate_pids

--- src/metax_api/management/commands/migrate_pids.py
+++ src/metax_api/management/commands/migrate_pids.py
@@ -11,8 +11,27 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    allow_fail = False
+    failed_pids = []
+    successful_pids = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.failed_pids = []
+        self.successful_pids = []
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--allow-fail",
+            required=False,
+            action="store_true",
+            default=False,
+            help="Allow individual PIDs to fail without halting the migration",
+        )
+
     def handle(self, *args, **options):
         logger.info("Migrating PIDs to PID MicroService")
+        self.allow_fail = options.get("allow_fail")
 
         pid_ms_client = PIDMSService()
         catalogs_to_migrate = settings.PID_MS["CATALOGS_TO_MIGRATE"]
@@ -26,6 +45,19 @@ class Command(BaseCommand):
         logger.info(f"Found {len(crs_to_migrate)} catalog record(s) to migrate")
 
         for cr in crs_to_migrate:
-            pid_ms_client.insert_pid(cr)
+            identifier = cr.identifier
+            try:
+                pid_ms_client.insert_pid(cr)
+                self.successful_pids.append(identifier)
+            except Exception as e:
+                if self.allow_fail:
+                    logger.info(repr(e))
+                    logger.info(f"Exception migrating PID {identifier}\n\n")
+                    self.failed_pids.append(identifier)
+                else:
+                    logger.error(f"Failed while processing {identifier}")
+                    raise
 
-        logger.info(f"Migrated {len(crs_to_migrate)} catalog record(s)")
+        logger.info(f"Succesfully migrated {len(self.successful_pids)} catalog record(s)")
+        if len(self.failed_pids) > 0:
+            logger.info(f"Failed to migrate pids for these datasets: {self.failed_pids}")
